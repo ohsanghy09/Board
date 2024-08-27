c18cf904a3494bafa85d9dcb710d44fb721c67bd
ohsanghy09 <ohsanghy09@naver.com>
2024-08-27 16:03:02 +0900

    add reply

---
+++ src/main/java/kr/ac/kopo/board/entity/Reply.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.board.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Reply extends BaseEntity{
+
+    @Id // 기본키 (primary key) 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1씩 자동 증가(auto-increment)
+    private Long rno;
+
+    private String text;
+
+    private String replyer;
+}
