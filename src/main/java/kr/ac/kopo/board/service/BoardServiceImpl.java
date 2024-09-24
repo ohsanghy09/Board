907b0bb7debfb37da64032fa7c992f050d2f31de
ohsanghy09 <ohsanghy09@naver.com>
2024-09-24 14:39:42 +0900

    BoardService에 getList() BoardServiceImpl에 getList() 구현

--- src/main/java/kr/ac/kopo/board/service/BoardServiceImpl.java
+++ src/main/java/kr/ac/kopo/board/service/BoardServiceImpl.java
@@ -1,10 +1,17 @@
 package kr.ac.kopo.board.service;
 
 import kr.ac.kopo.board.dto.BoardDTO;
+import kr.ac.kopo.board.dto.PageRequestDTO;
+import kr.ac.kopo.board.dto.PageResultDTO;
 import kr.ac.kopo.board.entity.Board;
+import kr.ac.kopo.board.entity.Member;
 import kr.ac.kopo.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +23,14 @@ public class BoardServiceImpl implements BoardService {
         Board board = dtoToEntity(dto);
         repository.save(board);
         return board.getBno();
+    }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board)en[0], (Member)en[1], (Long) en[2]));
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
