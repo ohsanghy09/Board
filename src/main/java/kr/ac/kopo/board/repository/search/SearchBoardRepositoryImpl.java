90538131d2f838c587405e78da2fe3b9434cbb50
ohsanghy09 <ohsanghy09@naver.com>
2024-10-22 15:58:27 +0900

    JPQL(Querydsl)을 사용한 검색기능 완성

--- src/main/java/kr/ac/kopo/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/kr/ac/kopo/board/repository/search/SearchBoardRepositoryImpl.java
@@ -2,7 +2,10 @@ package kr.ac.kopo.board.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import kr.ac.kopo.board.entity.Board;
 import kr.ac.kopo.board.entity.QBoard;
@@ -10,10 +13,13 @@ import kr.ac.kopo.board.entity.QMember;
 import kr.ac.kopo.board.entity.QReply;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hibernate.query.sqm.tree.SqmNode.log;
 
@@ -52,8 +58,8 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     }
 
     @Override
-    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
-        log.info("searchPage() 메소드 호출");
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable){
+        log.info("searPage() 메서드 호출됨");
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
         QMember member = QMember.member;
@@ -62,34 +68,53 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
         jpqlQuery.leftJoin(member).on(board.writer.eq(member));
         jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+//        tuple.groupBy(board, member, reply);
 
-        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count()).groupBy(board, member, reply);
-        tuple.groupBy(board, member, reply);
+
         BooleanBuilder booleanBuilder = new BooleanBuilder();
-        BooleanExpression expression = board.bno.gt(0L);
-        booleanBuilder.and(expression);
+        BooleanExpression booleanexpression = board.bno.gt(0L);
+        booleanBuilder.and(booleanexpression);
+
         if(type != null){
             String[] typeArr = type.split("");
             BooleanBuilder conditionBuilder = new BooleanBuilder();
 
-            for (String t : typeArr){
+            for(String t: typeArr){
                 switch (t){
                     case "t":
                         conditionBuilder.or(board.title.contains(keyword));
                         break;
+
                     case "w":
                         conditionBuilder.or(member.email.contains(keyword));
                         break;
                     case "c":
                         conditionBuilder.or(board.content.contains(keyword));
                         break;
-                }// end switch
-            }// end for
+                }//end switch
+            }//end for
             booleanBuilder.and(conditionBuilder);
-        }// end if
-    tuple.where(booleanBuilder);
-    tuple.groupBy(board, member);
-    List<Tuple>result = tuple.fetch();
-        return null;
-    }
-}
+        }//end if
+
+        tuple.where(booleanBuilder);
+        Sort sort = pageable.getSort();
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending()? Order.ASC:Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class, "board");
+
+            tuple.orderBy(new OrderSpecifier<Comparable>(direction, orderByExpression.get(prop)));
+        });
+        tuple.groupBy(board, member);
+
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+        List <Tuple> result = tuple.fetch();
+        log.info(result);
+        long count = tuple.fetchCount();
+        log.info("실행된 행의 개수: " + count);
+
+        return new PageImpl<Object[]>(result.stream().map(t->t.toArray()).collect(Collectors.toList()),pageable,count);
+    };}
